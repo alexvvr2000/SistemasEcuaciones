9409cae77fe44acdd53d7152d4e10372d0d81e84
mnpac <mnpac@ALE-LENOVO>
2024-03-01 22:10:50 -0600

    Add project files.

---
+++ Matrices/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matrizBase =
+            {
+                {2,3,4 },
+                {4,3,2 },
+                {0,1,0 }
+            };
+            Matriz matriz = new (matrizBase);
+            Matriz inversa = matriz.ObtenerInversa();
+            Console.WriteLine(inversa);
+        }
+    }
+}
---
+++ Matrices/SistemaEcuaciones.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Matrices
+{
+    readonly struct Matriz(double[,] arregloBase)
+    {
+        private readonly double[,] arreglo = arregloBase;
+        public readonly int Filas
+        {
+            get => arreglo.GetLength(0);
+        }
+        public readonly int Columnas
+        {
+            get => arreglo.GetLength(1);
+        }
+        public readonly Boolean EsCuadrada
+        {
+            get => Filas == Columnas;
+        }
+        public readonly int Orden
+        {
+            get {
+                if (Filas != Columnas)
+                {
+                    throw new Exception("La matriz no es cuadrada");
+                }
+                return arreglo.GetLength(0);
+            }
+        }
+        private double[,] ObtenerArregloBaseInversa()
+        {
+            double[,] arregloBaseInversa= new double[Filas, 2*Columnas];
+            for(int i = 0; i < Filas; i++)
+            {
+                for(int j= 0; j < Orden; j++)
+                {
+                    arregloBaseInversa[i, j] = arreglo[i, j];
+                    if (i == j) {
+                        arregloBaseInversa[i, j + Orden] = 1;
+                    }
+                }
+            }
+            return arregloBaseInversa;
+        }
+        public Matriz ObtenerInversa()
+        {
+            if (!EsCuadrada)
+            {
+                throw new Exception("Esta matriz no aplica para inversa");
+            }
+            double[,] inversa = this.ObtenerArregloBaseInversa();
+            return new Matriz(inversa);
+        }
+        public override string ToString()
+        {
+            StringBuilder arregloString = new();
+            for(int i = 0; i < Filas;i++) {
+                for(int j = 0; j < Columnas;j++)
+                {
+                    arregloString.Append($"{arreglo[i, j]},");
+                }
+                arregloString.Remove(arregloString.Length - 1,1);
+                arregloString.Append('\n');
+            }
+            arregloString.Remove(arregloString.Length - 1, 1);
+            return arregloString.ToString();
+        }
+    }
+}
