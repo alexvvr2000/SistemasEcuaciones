543e73ab57406ce86aa6a1326a17e8d09057121c
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2024-03-03 14:38:40 -0600

    Implementado iterador de valores double en matriz

--- Matrices/SistemaEcuaciones.cs
+++ Matrices/SistemaEcuaciones.cs
@@ -1,8 +1,9 @@
-﻿using System.Text;
+﻿using System.Collections;
+using System.Text;
 
 namespace Matrices
 {
-    readonly struct Matriz
+    readonly struct Matriz: IEnumerable<Double>
     {
         private readonly Double[][] matrizBase;
         public readonly Int32 numeroFilas;
@@ -187,6 +188,24 @@ namespace Matrices
             }
             arregloString.Remove(arregloString.Length - 1, 1);
             return arregloString.ToString();
+        }
+        private IEnumerator<Double> ValoresMatriz()
+        {
+            for (int i = 0; i < numeroFilas; i++)
+            {
+                for (int j = 0; j < numeroColumnas; j++)
+                {
+                    yield return this[i, j];
+                }
+            }
+        }
+        public IEnumerator<Double> GetEnumerator()
+        {
+            return this.ValoresMatriz();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
         }
     }
 }
