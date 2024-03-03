214e33909264343391fba166fd79aac030f9c27e
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2024-03-03 14:53:56 -0600

    Agregados iteradores base para cada matrices

---
+++ Matrices/Iteradores/IteradorColumnas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SistemaEcuaciones.IteradoresMatrices
+{
+    class IteradorColumnas : IEnumerable<Double>
+    {
+        public IEnumerator<double> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Matrices/Iteradores/IteradorFilas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEcuaciones.IteradoresMatrices
+{
+    class IteradorFilas : IEnumerable<Double>
+    {
+        public IEnumerator<double> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
