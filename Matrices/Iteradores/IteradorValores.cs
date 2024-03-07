5e0952c05605b69e7b695440f70137ef1442b531
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2024-03-07 09:07:09 -0600

    Cambiado iterador de valores a carpeta iteradores

---
+++ Matrices/Iteradores/IteradorValores.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SistemaEcuaciones
+{
+    public partial struct Matriz : IEnumerable<Double>
+    {
+        private IEnumerator<Double> ValoresMatriz()
+        {
+            for (int i = 0; i < numeroFilas; i++)
+            {
+                for (int j = 0; j < numeroColumnas; j++)
+                {
+                    yield return this[i, j];
+                }
+            }
+        }
+        public IEnumerator<Double> GetEnumerator()
+        {
+            return this.ValoresMatriz();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
