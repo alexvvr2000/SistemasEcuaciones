7f3696cadc9f3d098c94b2f9de1c1869d35975cc
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2024-03-10 21:25:13 -0600

    creadas clases base

---
+++ Matrices/MetodosSistemas/Jacobi.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace SistemaEcuaciones.MetodosSistemas
+{
+    record ResultadoIteracion(Int32 iteracion,ImmutableArray<double> componentes,ImmutableDictionary<int, double> errorRelativoComponente);
+    public class Jacobi
+    {
+    }
+}
