0ead3cd7704b403cb4efa9c358ef2a6ab09a2526
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2024-03-13 09:53:08 -0600

    implementados metodos para obtener una matriz diagonal con respecto a otro y una para obtener todos los valores menos la diagonal

--- Matrices/Matriz/Constructores.cs
+++ Matrices/Matriz/Constructores.cs
@@ -60,5 +60,29 @@ namespace SistemaEcuaciones
         {
             matrizOriginal.CopyTo(matrizBase[0], 0);
         }
+        public Matriz obtenerMatrizDiagonal()
+        {
+            if (!this.esCuadrada) throw new InvalidOperationException("La matriz debe ser cuadrada");
+            Matriz nuevaMatriz = new Matriz(this.numeroColumnas, this.numeroColumnas);
+            for (int i = 0; i < this.orden; i++)
+            {
+                nuevaMatriz[i, i] = this[i, i];
+            }
+            return nuevaMatriz;
+        }
+        public Matriz obtenerMatrizNoDiagonal()
+        {
+            if (!this.esCuadrada) throw new InvalidOperationException("La matriz debe ser cuadrada");
+            Matriz nuevaMatriz = new Matriz(this.numeroColumnas, this.numeroColumnas);
+            for (int i = 0; i <= (this.numeroFilas - 1); i++)
+            {
+                for (int j = i+1; j < this.numeroColumnas; j++)
+                {
+                    nuevaMatriz[i, j] = this[i, j];
+                    nuevaMatriz[j, i] = this[j, i];
+                }
+            }
+            return nuevaMatriz;
+        }
     }
 }
