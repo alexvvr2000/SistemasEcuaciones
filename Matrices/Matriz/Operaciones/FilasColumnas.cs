7e8ebb3c24a817fcafa25256c4d1c6b56300340f
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-05 17:03:42 -0600

    FilasColumnas ahora no usa tipos primitivos de System

--- Matrices/Matriz/Operaciones/FilasColumnas.cs
+++ Matrices/Matriz/Operaciones/FilasColumnas.cs
@@ -1,75 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace SistemaEcuaciones
+﻿namespace SistemaEcuaciones
 {
-    partial struct Matriz:ICloneable
+    partial struct Matriz : ICloneable
     {
-        public Dictionary<string, Func<Matriz, Decimal>> obtenerFuncionesDimensiones(Matriz valoresDimensionAlta)
+        public readonly Dictionary<string, Func<Matriz, decimal>> ObtenerFuncionesDimensiones(Matriz valoresDimensionAlta)
         {
-            if (!this.esCuadrada) throw new InvalidOperationException("La matriz debe ser cuadrada");
-            if (!valoresDimensionAlta.esMatrizColumna) throw new InvalidOperationException($"Los valores de la dimension {this.orden + 1} debe ser una matriz columna");
-            if (valoresDimensionAlta.numeroFilas != this.numeroColumnas) throw new InvalidOperationException($"Los valores asignados a la dimension {this.orden + 1} deben ser {this.numeroColumnas}");
-            Dictionary<string, Func<Matriz, Decimal>> funciones = new Dictionary<string, Func<Matriz, Decimal>>();
-            for (int i = 0; i < this.orden; i++)
+            if (!EsCuadrada) throw new InvalidOperationException("La matriz debe ser cuadrada");
+            if (!valoresDimensionAlta.EsMatrizColumna) throw new InvalidOperationException($"Los valores de la dimension {Orden + 1} debe ser una matriz columna");
+            if (valoresDimensionAlta.numeroFilas != numeroColumnas) throw new InvalidOperationException($"Los valores asignados a la dimension {Orden + 1} deben ser {numeroColumnas}");
+            Dictionary<string, Func<Matriz, decimal>> funciones = new();
+            for (int i = 0; i < Orden; i++)
             {
-                Matriz filaEvaluada = this.ObtenerFila(i);
-                Int32 numeroComponente = i;
-                Decimal valorDimensionActual = valoresDimensionAlta[i, 0];
+                Matriz filaEvaluada = ObtenerFila(i);
+                int numeroComponente = i;
+                decimal valorDimensionActual = valoresDimensionAlta[i, 0];
                 funciones.Add($"x{i}", matrizResultados =>
                 {
-                    if (!matrizResultados.esMatrizColumna) throw new InvalidOperationException($"El vector a evaluar debe ser un vector columna");
+                    if (!matrizResultados.EsMatrizColumna) throw new InvalidOperationException($"El vector a evaluar debe ser un vector columna");
                     if (filaEvaluada.numeroFilas != matrizResultados.numeroColumnas) throw new InvalidOperationException($"El vector debe tener {filaEvaluada.numeroColumnas} valores con x{numeroComponente} con un valor arbitrario");
-                    return (valorDimensionActual - ((filaEvaluada * matrizResultados)[0, 0] - (filaEvaluada[0, numeroComponente] * matrizResultados[numeroComponente, 0]))) / filaEvaluada[0,numeroComponente];
+                    return (valorDimensionActual - ((filaEvaluada * matrizResultados)[0, 0] - (filaEvaluada[0, numeroComponente] * matrizResultados[numeroComponente, 0]))) / filaEvaluada[0, numeroComponente];
                 });
             }
             return funciones;
         }
-        public Matriz CambiarColumna(Int32 columnaOrigen,Int32 columnaDestino)
+        public Matriz CambiarColumna(int columnaOrigen, int columnaDestino)
         {
-            bool origenValido = this.IndiceValido(0, columnaOrigen);
-            bool destinoValido = this.IndiceValido(0, columnaDestino);
+            bool origenValido = IndiceValido(0, columnaOrigen);
+            bool destinoValido = IndiceValido(0, columnaDestino);
             if (!(origenValido && destinoValido))
             {
                 throw new IndexOutOfRangeException("Indice de fila no es valido");
             }
-            for (int i = 0; i < this.numeroFilas; i++)
+            for (int i = 0; i < numeroFilas; i++)
             {
-                Decimal valorTemporal = this[i,columnaDestino];
-                this[i, columnaDestino] = this[i, columnaOrigen];
-                this[i, columnaOrigen] = valorTemporal;
+                (this[i, columnaOrigen], this[i, columnaDestino]) = (this[i, columnaDestino], this[i, columnaOrigen]);
             }
             return this;
         }
-        public Matriz CambiarFila(Int32 filaOrigen, Int32 filaDestino)
+        public readonly Matriz CambiarFila(int filaOrigen, int filaDestino)
         {
-            bool origenValido = this.IndiceValido(filaOrigen,0);
-            bool destinoValido = this.IndiceValido(filaDestino,0);
+            bool origenValido = IndiceValido(filaOrigen, 0);
+            bool destinoValido = IndiceValido(filaDestino, 0);
             if (!(origenValido && destinoValido))
             {
                 throw new IndexOutOfRangeException("Indice de fila no es valido");
             }
-            Decimal[] filaTemporal = this.matrizBase[filaDestino];
-            this.matrizBase[filaDestino] = this.matrizBase[filaOrigen];
-            this.matrizBase[filaOrigen] = filaTemporal;
+            (matrizBase[filaOrigen], matrizBase[filaDestino]) = (matrizBase[filaDestino], matrizBase[filaOrigen]);
             return this;
         }
-        public Matriz CambiarFila(Decimal[] filaNueva, Int32 indiceFila)
+        public readonly Matriz CambiarFila(decimal[] filaNueva, int indiceFila)
         {
-            if (!this.IndiceValido(indiceFila, 0))
+            if (!IndiceValido(indiceFila, 0))
             {
                 throw new IndexOutOfRangeException("Indice de fila no es valido");
             }
-            if (filaNueva.Length != this.numeroColumnas)
+            if (filaNueva.Length != numeroColumnas)
             {
-                throw new InvalidOperationException($"La nueva columna debe ser de tamaño {this.numeroColumnas}");
+                throw new InvalidOperationException($"La nueva columna debe ser de tamaño {numeroColumnas}");
             }
-            filaNueva.CopyTo(this.matrizBase[indiceFila], 0);
+            filaNueva.CopyTo(matrizBase[indiceFila], 0);
             return this;
         }
-        public Matriz CambiarFila(Matriz nuevaFila, Int32 indiceFila)
+        public readonly Matriz CambiarFila(Matriz nuevaFila, int indiceFila)
         {
-            if (!this.IndiceValido(indiceFila, 0))
+            if (!IndiceValido(indiceFila, 0))
             {
                 throw new IndexOutOfRangeException("Indice de fila no es valido");
             }
@@ -77,46 +70,46 @@ namespace SistemaEcuaciones
             {
                 throw new InvalidOperationException($"Solo puede asignar matrices renglon");
             }
-            if (nuevaFila.numeroColumnas != this.numeroColumnas)
+            if (nuevaFila.numeroColumnas != numeroColumnas)
             {
-                throw new InvalidOperationException($"La nueva columna debe ser de tamaño {this.numeroColumnas}");
+                throw new InvalidOperationException($"La nueva columna debe ser de tamaño {numeroColumnas}");
             }
             for (int i = 0; i < nuevaFila.numeroColumnas; i++)
             {
-                this.matrizBase[indiceFila][i] = nuevaFila[0, i];
+                matrizBase[indiceFila][i] = nuevaFila[0, i];
             }
             return this;
         }
-        public Matriz MultiplicarFila(Int32 indiceFila, Decimal multiplo)
+        public readonly Matriz MultiplicarFila(int indiceFila, decimal multiplo)
         {
-            if (!this.IndiceValido(indiceFila, 0))
+            if (!IndiceValido(indiceFila, 0))
             {
                 throw new IndexOutOfRangeException("Indice de fila no es valido");
             }
-            for (Int32 i = 0; i < numeroColumnas; i++)
+            for (int i = 0; i < numeroColumnas; i++)
             {
-                this.matrizBase[indiceFila][i] *= multiplo;
+                matrizBase[indiceFila][i] *= multiplo;
             }
 
             return this;
         }
-        public Matriz SumarFilas(Int32 indiceFilaOrigen, Int32 indiceFilaDestino, Decimal multiplo)
+        public readonly Matriz SumarFilas(int indiceFilaOrigen, int indiceFilaDestino, decimal multiplo)
         {
-            Boolean filaOrigenValida = this.IndiceValido(indiceFilaOrigen, 0);
-            Boolean filaDestinoValido = this.IndiceValido(indiceFilaDestino, 0);
+            bool filaOrigenValida = IndiceValido(indiceFilaOrigen, 0);
+            bool filaDestinoValido = IndiceValido(indiceFilaDestino, 0);
             if (!(filaDestinoValido && filaOrigenValida))
             {
                 throw new IndexOutOfRangeException("Indice de fila no es valido");
             }
-            for (Int32 i = 0; i < numeroColumnas; i++)
+            for (int i = 0; i < numeroColumnas; i++)
             {
-                this.matrizBase[indiceFilaDestino][i] += multiplo * this.matrizBase[indiceFilaOrigen][i];
+                matrizBase[indiceFilaDestino][i] += multiplo * matrizBase[indiceFilaOrigen][i];
             }
             return this;
         }
-        public object Clone()
+        public readonly object Clone()
         {
-            return new Matriz(this.matrizBase);
+            return new Matriz(matrizBase);
         }
     }
 }
