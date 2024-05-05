a9a30a560f02af57c068347d20c1b973afcc5278
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-05 17:03:14 -0600

    Propiedades ahora no usa tipos primitivos de System

--- Matrices/Matriz/Propiedades.cs
+++ Matrices/Matriz/Propiedades.cs
@@ -1,146 +1,130 @@
-﻿using System;
-
-namespace SistemaEcuaciones
+﻿namespace SistemaEcuaciones
 {
     public partial struct Matriz
     {
-        private readonly Decimal[][] matrizBase;
-        public readonly Int32 numeroFilas;
-        public readonly Int32 numeroColumnas;
-        private Boolean IndiceValido(Int32 fila, Int32 columna)
+        private readonly decimal[][] matrizBase;
+        public readonly int numeroFilas;
+        public readonly int numeroColumnas;
+        private readonly bool IndiceValido(int fila, int columna)
         {
-            Boolean filaValida = fila >= 0 && fila < this.numeroFilas;
-            Boolean columnaValida = columna >= 0 && columna < this.numeroColumnas;
+            bool filaValida = fila >= 0 && fila < numeroFilas;
+            bool columnaValida = columna >= 0 && columna < numeroColumnas;
             return filaValida && columnaValida;
         }
-        public Boolean esMatrizColumna
+        public readonly bool EsMatrizColumna => numeroColumnas == 1;
+        public readonly bool EsMatrizRenglon => numeroFilas == 1;
+        public readonly bool EsCuadrada => numeroColumnas == numeroFilas;
+        public readonly int Orden
         {
             get
             {
-                return this.numeroColumnas == 1;
-            }
-        }
-        public Boolean esMatrizRenglon
-        {
-            get
-            {
-                return this.numeroFilas == 1;
-            }
-        }
-        public Boolean esCuadrada
-        {
-            get => numeroColumnas == numeroFilas;
-        }
-        public Int32 orden
-        {
-            get
-            {
-                if (!esCuadrada) throw new InvalidOperationException("La matriz no es cuadrada");
+                if (!EsCuadrada) throw new InvalidOperationException("La matriz no es cuadrada");
                 return numeroFilas;
             }
         }
-        public Boolean esDominante
+        public readonly bool EsDominante
         {
             get
             {
-                if (!esCuadrada) throw new InvalidOperationException("La matriz no es cuadrada");
-                for (int i = 0; i < this.numeroFilas; i++)
+                if (!EsCuadrada) throw new InvalidOperationException("La matriz no es cuadrada");
+                for (int i = 0; i < numeroFilas; i++)
                 {
-                    Decimal valorComparado = Math.Abs(this[i, i]);
-                    if (!(valorComparado > this.sumaFilaAbsoluta(i, i)))
+                    decimal valorComparado = Math.Abs(this[i, i]);
+                    if (!(valorComparado > SumaFilaAbsoluta(i, i)))
                     {
-                        Console.WriteLine($"Numero: {valorComparado}, Suma: {this.sumaFilaAbsoluta(i,i)}");
+                        Console.WriteLine($"Numero: {valorComparado}, Suma: {SumaFilaAbsoluta(i, i)}");
                         return false;
                     }
                 }
                 return true;
             }
         }
-        public Decimal determinante
+        public readonly decimal Determinante
         {
             get
             {
-                if (!esCuadrada) throw new InvalidOperationException("La matriz no es cuadrada");
-                Matriz copia = (Matriz)this.Clone();
-                Decimal determinanteTotal = 1;
+                if (!EsCuadrada) throw new InvalidOperationException("La matriz no es cuadrada");
+                Matriz copia = (Matriz)Clone();
+                decimal determinanteTotal = 1;
                 for (int i = 0; i < copia.numeroColumnas; i++)
                 {
-                    Decimal valorDiagonal = copia[i, i];
-                    if(valorDiagonal == 0)
+                    decimal valorDiagonal = copia[i, i];
+                    if (valorDiagonal == 0)
                     {
-                        Int32? filaCambio = copia.ObtenerFilaValidaAbajo(i,i);
+                        int? filaCambio = copia.ObtenerFilaValidaAbajo(i, i);
                         if (!filaCambio.HasValue) return 0;
                         copia.CambiarColumna(filaCambio.Value, i);
-                        valorDiagonal = copia[i,i];
+                        valorDiagonal = copia[i, i];
                     }
                     for (int j = i + 1; j < copia.numeroFilas; j++)
                     {
-                        Decimal escalar = -copia[j, i] / valorDiagonal;
-                        copia.SumarFilas(i,j,escalar);
+                        decimal escalar = -copia[j, i] / valorDiagonal;
+                        copia.SumarFilas(i, j, escalar);
                     }
                     determinanteTotal *= valorDiagonal;
                 }
                 return determinanteTotal;
             }
         }
-        private Int32? ObtenerFilaValidaAbajo(Int32 filaInicial, Int32 columnaBusqueda)
+        private readonly int? ObtenerFilaValidaAbajo(int filaInicial, int columnaBusqueda)
         {
-            if (!this.esCuadrada) throw new InvalidOperationException("La matriz no es cuadrada");
-            if (!this.IndiceValido(filaInicial, columnaBusqueda)) throw new IndexOutOfRangeException("No se introdujo una coordenada valida");
-            if (filaInicial == (this.numeroFilas - 1)) return null;
-            for (int i = filaInicial; i < this.orden; i++)
+            if (!EsCuadrada) throw new InvalidOperationException("La matriz no es cuadrada");
+            if (!IndiceValido(filaInicial, columnaBusqueda)) throw new IndexOutOfRangeException("No se introdujo una coordenada valida");
+            if (filaInicial == (numeroFilas - 1)) return null;
+            for (int i = filaInicial; i < Orden; i++)
             {
                 if (this[i, columnaBusqueda] != 0) return i;
             }
             return null;
         }
-        private Decimal sumaFilaAbsoluta(Int32 filaObjetivo, Int32 columnaExcluida)
+        private readonly decimal SumaFilaAbsoluta(int filaObjetivo, int columnaExcluida)
         {
-            Decimal sumaAbsoluta = 0;
-            for (int i = 0; i < this.numeroColumnas; i++)
+            decimal sumaAbsoluta = 0;
+            for (int i = 0; i < numeroColumnas; i++)
             {
                 if (i == columnaExcluida) continue;
-                sumaAbsoluta += Math.Abs(this[filaObjetivo,i]);
+                sumaAbsoluta += Math.Abs(this[filaObjetivo, i]);
             }
             return sumaAbsoluta;
         }
-        public Decimal this[Int32 indiceFila, Int32 indiceColumna]
+        public readonly decimal this[int indiceFila, int indiceColumna]
         {
             get
             {
-                if (!this.IndiceValido(indiceFila, indiceColumna))
+                if (!IndiceValido(indiceFila, indiceColumna))
                 {
                     throw new IndexOutOfRangeException("Indice de fila no es valido");
                 }
-                return this.matrizBase[indiceFila][indiceColumna];
+                return matrizBase[indiceFila][indiceColumna];
             }
+
             set
             {
-                if (!this.IndiceValido(indiceFila, indiceColumna))
+                if (!IndiceValido(indiceFila, indiceColumna))
                 {
                     throw new IndexOutOfRangeException("Indice de fila no es valido");
                 }
-                this.matrizBase[indiceFila][indiceColumna] = value;
+                matrizBase[indiceFila][indiceColumna] = value;
             }
         }
-        public Matriz ObtenerFila(Int32 indiceFila)
+        public readonly Matriz ObtenerFila(int indiceFila)
         {
-            if (!this.IndiceValido(indiceFila, 0))
+            if (!IndiceValido(indiceFila, 0))
             {
                 throw new IndexOutOfRangeException("Indice de fila no es valido");
             }
-            return new Matriz(this.matrizBase[indiceFila]);
+            return new Matriz(matrizBase[indiceFila]);
         }
-        public Matriz ObtenerColumna(Int32 indiceColumna)
+        public readonly Matriz ObtenerColumna(int indiceColumna)
         {
-            if (!this.IndiceValido(0, indiceColumna))
+            if (!IndiceValido(0, indiceColumna))
             {
-                throw new IndexOutOfRangeException($"El indice debe estar entre 0 y {this.numeroColumnas - 1}");
+                throw new IndexOutOfRangeException($"El indice debe estar entre 0 y {numeroColumnas - 1}");
             }
-            Matriz nuevaMatriz = new Matriz(this.numeroFilas, 1);
-            for (int i = 0; i < this.numeroFilas; i++)
+            Matriz nuevaMatriz = new(numeroFilas, 1);
+            for (int i = 0; i < numeroFilas; i++)
             {
-                nuevaMatriz[i, 0] = this.matrizBase[i][indiceColumna];
+                nuevaMatriz[i, 0] = matrizBase[i][indiceColumna];
             }
             return nuevaMatriz;
         }
