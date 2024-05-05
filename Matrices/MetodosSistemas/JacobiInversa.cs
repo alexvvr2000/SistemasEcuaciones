44ada774881657be64c6e88255fc07cf6fa15c7b
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-05 17:04:06 -0600

    JacobiInversa ahora no usa tipos primitivos de System

--- Matrices/MetodosSistemas/JacobiInversa.cs
+++ Matrices/MetodosSistemas/JacobiInversa.cs
@@ -1,46 +1,43 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Immutable;
 
 namespace SistemaEcuaciones.MetodosSistemas
 {
-    public record ResultadoIteracionJacobiInversa(Int32 iteracion, Matriz nuevaAproximacion, ImmutableDictionary<int, Decimal> errorRelativoComponente);
+    public record ResultadoIteracionJacobiInversa(int iteracion, Matriz nuevaAproximacion, ImmutableDictionary<int, decimal> errorRelativoComponente);
     public class JacobiInversa : IEnumerable<ResultadoIteracionJacobiInversa>
     {
-        private readonly Matriz matrizSistema;
         private readonly Matriz matrizRenglonesNoDiagonales;
         private readonly Matriz matrizInversaDiagonal;
         private readonly Matriz valorInicial;
         private readonly Matriz matrizResultados;
-        private readonly Int32 iteraciones;
-        public JacobiInversa(Matriz matrizSistema, Matriz matrizResultados, Matriz valorInicial, Int32 iteraciones)
+        private readonly int iteraciones;
+        public JacobiInversa(Matriz matrizSistema, Matriz matrizResultados, Matriz valorInicial, int iteraciones)
         {
-            if (matrizSistema.determinante == 0) throw new ArgumentException($"El sistema no esta definido en R{matrizSistema.orden}");
-            if (!matrizSistema.esDominante) throw new ArgumentException("La matriz no es dominante");
+            if (matrizSistema.Determinante == 0) throw new ArgumentException($"El sistema no esta definido en R{matrizSistema.Orden}");
+            if (!matrizSistema.EsDominante) throw new ArgumentException("La matriz no es dominante");
             if (matrizResultados.numeroFilas != matrizSistema.numeroFilas) throw new ArgumentException($"El vector de resultados debe tener {matrizSistema.numeroFilas} filas");
-            if (!matrizResultados.esMatrizColumna) throw new ArgumentException("Solo puede usarse con un solo vector de resultados");
-            if (!valorInicial.esMatrizColumna) throw new ArgumentException("El valor inicial debe ser un vector columna");
+            if (!matrizResultados.EsMatrizColumna) throw new ArgumentException("Solo puede usarse con un solo vector de resultados");
+            if (!valorInicial.EsMatrizColumna) throw new ArgumentException("El valor inicial debe ser un vector columna");
             if (valorInicial.numeroFilas != matrizSistema.numeroColumnas) throw new ArgumentException("La cantidad de componentes no es igual a las dimensiones del sistema");
-            if (iteraciones <= 0) throw new ArgumentOutOfRangeException("Debe ser 1 o mas iteraciones");
-            this.matrizSistema = matrizSistema;
-            this.matrizRenglonesNoDiagonales = matrizSistema.obtenerMatrizNoDiagonal();
-            this.matrizInversaDiagonal = matrizSistema.obtenerMatrizDiagonal().ObtenerInversa()
-                ?? throw new ArgumentException($"El sistema no esta definido en R{matrizSistema.orden}");;
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(iteraciones);
+            matrizRenglonesNoDiagonales = matrizSistema.obtenerMatrizNoDiagonal();
+            matrizInversaDiagonal = matrizSistema.obtenerMatrizDiagonal().ObtenerInversa()
+                ?? throw new ArgumentException($"El sistema no esta definido en R{matrizSistema.Orden}"); ;
             this.valorInicial = valorInicial;
             this.matrizResultados = matrizResultados;
             this.iteraciones = iteraciones;
         }
-        private IEnumerator<ResultadoIteracionJacobiInversa> aproximarFuncion()
+        private IEnumerator<ResultadoIteracionJacobiInversa> AproximarFuncion()
         {
-            Matriz valorAproximadoAnterior = this.valorInicial;
-            var erroresRelativos = ImmutableDictionary.CreateBuilder<int, Decimal>();
-            for (int i = 0; i < this.iteraciones; i++)
+            Matriz valorAproximadoAnterior = valorInicial;
+            var erroresRelativos = ImmutableDictionary.CreateBuilder<int, decimal>();
+            for (int i = 0; i < iteraciones; i++)
             {
-                Matriz valorAproximadoActual = this.matrizInversaDiagonal*(this.matrizResultados - this.matrizRenglonesNoDiagonales*valorAproximadoAnterior);
+                Matriz valorAproximadoActual = matrizInversaDiagonal * (matrizResultados - matrizRenglonesNoDiagonales * valorAproximadoAnterior);
                 for (int j = 0; j < valorAproximadoActual.numeroFilas; j++)
                 {
-                    erroresRelativos[j] = i == 0 ? -1 
-                        : Math.Abs(valorAproximadoActual[j,0] - valorAproximadoAnterior[j,0]) / Math.Abs(valorAproximadoActual[j,0]);
+                    erroresRelativos[j] = i == 0 ? -1
+                        : Math.Abs(valorAproximadoActual[j, 0] - valorAproximadoAnterior[j, 0]) / Math.Abs(valorAproximadoActual[j, 0]);
                 }
                 yield return new ResultadoIteracionJacobiInversa(i, valorAproximadoActual, erroresRelativos.ToImmutable());
                 valorAproximadoAnterior = valorAproximadoActual;
@@ -48,11 +45,11 @@ namespace SistemaEcuaciones.MetodosSistemas
         }
         public IEnumerator<ResultadoIteracionJacobiInversa> GetEnumerator()
         {
-            return this.aproximarFuncion();
+            return AproximarFuncion();
         }
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return this.GetEnumerator();
+            return GetEnumerator();
         }
     }
 }
