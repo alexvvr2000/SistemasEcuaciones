c87a5601a58bda7c3374cb40cffaab432dea3c01
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-05 23:58:10 -0600

    Creado iterador base vandermode

--- Matrices/Vandermonde.cs
+++ Matrices/Vandermonde.cs
@@ -1,6 +1,24 @@
-﻿namespace SistemaEcuaciones;
+﻿using static SistemaEcuaciones.IteradorMatriz;
+namespace SistemaEcuaciones;
 
 public class Vandermonde
 {
+    public record PuntoPolinomio(decimal ValorX, decimal ValorY);
+    private readonly Matriz Coeficientes;
+    public Vandermonde(PuntoPolinomio[] listaPuntos)
+    {
+        if (listaPuntos.Length <= 1) throw new ArgumentOutOfRangeException(paramName: nameof(listaPuntos), message: "Se debe tener 2 puntos o mas");
+        Coeficientes = ObtenerCoordenadasColumnas(new Matriz(listaPuntos.Length, listaPuntos.Length))
+        .Aggregate(
+            new Matriz(listaPuntos.Length, listaPuntos.Length),
+            (Matriz matrizCalculada, CoordenadaMatriz[] coordenadasFilasN) =>
+        {
+            foreach (CoordenadaMatriz coordenada in coordenadasFilasN)
+            {
 
+            }
+            return matrizCalculada;
+        });
+        Console.WriteLine(Coeficientes);
+    }
 }
